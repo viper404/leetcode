c2a7c79360be9d03f593a77a8b04745be3c821f2
vincent peng <viper404@163.com>
2016-07-31 00:15:00 +0800

    Update solution.js

--- 2.AddTwoNumbers/solution.js
+++ 2.AddTwoNumbers/solution.js
@@ -1,3 +1,4 @@
+// 2. Add Two Numbers
 // You are given two linked lists representing two non-negative numbers.
 // The digits are stored in reverse order and each of their nodes contain a single digit.
 // Add the two numbers and return it as a linked list.
