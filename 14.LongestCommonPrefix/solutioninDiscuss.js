e1ed61a4ce7487dffd174bb68082b1d527db6413
vincent peng <viper404@163.com>
2016-08-24 16:54:15 +0800

    Add files via upload

---
+++ 14.LongestCommonPrefix/solutioninDiscuss.js
@@ -0,0 +1,20 @@
+// 14. Longest Common Prefix
+// Write a function to find the longest common prefix string amongst an array of strings.
+// /**
+//  * @param {string[]} strs
+//  * @return {string}
+//  */
+var longestCommonPrefix = function(strs) {
+  if (strs.length === 0) {
+    return "";
+  }
+  var str = strs[0];
+  for (var i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(str) !== 0) {
+      str = str.substring(0, str.length-1);
+    }
+  }
+  return str;
+}
+var strs = [];
+console.log(longestCommonPrefix(strs));
