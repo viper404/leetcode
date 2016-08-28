f090efc9a5c300d97bdffb6cae92570d2bec0606
vincent peng <viper404@163.com>
2016-08-28 13:32:22 +0800

    Add files via upload

---
+++ 26.RemoveDuplicatesfromSortedArray/solutioninDiscuss.js
@@ -0,0 +1,22 @@
+// 26. Remove Duplicates from Sorted Array
+// Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+// Do not allocate extra space for another array, you must do this in place with constant memory.
+// For example,
+// Given input array nums = [1,1,2],
+// Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+// It doesn't matter what you leave beyond the new length.
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+var removeDuplicates = function(nums) {
+  var i = (nums.length > 0) ? 1 : 0;
+  for (var j in nums) {
+    if (nums[j] > nums[i-1]) {
+      nums[i++] = nums[j];
+    }
+  }
+  return i;
+}
+var nums = [];
+console.log(removeDuplicates(nums));
