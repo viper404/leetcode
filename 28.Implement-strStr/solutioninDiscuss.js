9ec9bf5ea7bebfb80b0432ad533730a5f06b1887
vincent peng <viper404@163.com>
2016-08-31 10:58:12 +0800

    Add files via upload

---
+++ 28.Implement-strStr/solutioninDiscuss.js
@@ -0,0 +1,26 @@
+// 28. Implement strStr()
+// Implement strStr().
+// Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+// /**
+//  * @param {string} haystack
+//  * @param {string} needle
+//  * @return {number}
+//  */
+var strStr = function(haystack, needle) {
+  for (var i = 0; ; i++) {
+    for (var j = 0; ; j++) {
+      if (j === needle.length) {
+        return i;
+      }
+      if (i+j === haystack.length) {
+        return -1;
+      }
+      if (needle[j] !== haystack[i+j]) {
+        break;
+      }
+    }
+  }
+}
+var haystack = "";
+var needle = "";
+console.log(strStr(haystack, needle));
