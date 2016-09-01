85277869a84a9c3d5f769c70050d7ba49cc10dec
vincent peng <viper404@163.com>
2016-09-01 11:34:23 +0800

    Update solution.js

--- 34.SearchforaRange/solution.js
+++ 34.SearchforaRange/solution.js
@@ -1,39 +1,23 @@
-// 1. Two Sum
-// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-// You may assume that each input would have exactly one solution.
-// Example:
-// Given nums = [2, 7, 11, 15], target = 9,
-// Because nums[0] + nums[1] = 2 + 7 = 9,
-// return [0, 1].
+// 34. Search for a Range
+// Given a sorted array of integers, find the starting and ending position of a given target value.
+// If the target is not found in the array, return [-1, -1].
+// For example,
+// Given [5, 7, 7, 8, 8, 10] and target value 8,
+// return [3, 4].
 // /**
 //  * @param {number[]} nums
 //  * @param {number} target
 //  * @return {number[]}
 //  */
-// UPDATE (2016/2/13):
-// The return format had been changed to zero-based indices. Please read the above updated description carefully.
-var twoSum = function(nums, target) {
-  var first = 0;
-  var second = 0;
-  var map = new Map();
-  for (var i = 0; i < nums.length; i++) {
-    if ((map.has(nums[i])) && (nums[i] === target/2)) {
-      second = i;
-      first = map.get(target-nums[i]);
-    }
-    else {
-      map.set(nums[i], i);
-      if (map.has(target-nums[i])) {
-        second = i;
-        if (second !== map.get(target-nums[i])) {
-          first = map.get(target-nums[i]);
-          break;
-        }
-      }
-    }
+var searchRange = function(nums, target) {
+  var i = 0;
+  var j = nums.length-1;
+  while (nums[i] < nums[j]) {
+    i = (nums[i] === target) ? i : i+1;
+    j = (nums[j] === target) ? j : j-1;
   }
-  return [first, second];
+  return (nums[i] === target) ? [i, j] : [-1, -1];
 }
-var nums = [2, 7, 11, 15];
-var target = 9;
-console.log(twoSum(nums, target));
+var nums = [1];
+var target = 0;
+console.log(searchRange(nums, target));
