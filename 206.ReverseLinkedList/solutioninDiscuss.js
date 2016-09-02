5975014f4f055fc12777d40d1541d32dc2144865
vincent peng <viper404@163.com>
2016-09-03 00:28:44 +0800

    Add files via upload

---
+++ 206.ReverseLinkedList/solutioninDiscuss.js
@@ -0,0 +1,26 @@
+// 206. Reverse Linked List
+// Reverse a singly linked list.
+// Hint:
+// A linked list can be reversed either iteratively or recursively. Could you implement both?
+// /**
+//  * Definition for singly-linked list.
+//  * function ListNode(val) {
+//  *     this.val = val;
+//  *     this.next = null;
+//  * }
+//  */
+// /**
+//  * @param {ListNode} head
+//  * @return {ListNode}
+//  */
+var reverseList = function(head) {
+  return helper(head, null);
+}
+function helper(head, left) {
+  if (head === null) {
+    return left;
+  }
+  var right = head.next;
+  head.next = left;
+  return helper(right, head);
+}
