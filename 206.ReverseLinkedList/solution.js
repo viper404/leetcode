e3dfb7e9fc8086b30547da243ecfd60ba080fc79
vincent peng <viper404@163.com>
2016-09-03 00:27:39 +0800

    Update solution.js

--- 206.ReverseLinkedList/solution.js
+++ 206.ReverseLinkedList/solution.js
@@ -1,39 +1,28 @@
-// 1. Two Sum
-// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-// You may assume that each input would have exactly one solution.
-// Example:
-// Given nums = [2, 7, 11, 15], target = 9,
-// Because nums[0] + nums[1] = 2 + 7 = 9,
-// return [0, 1].
+// 206. Reverse Linked List
+// Reverse a singly linked list.
+// Hint:
+// A linked list can be reversed either iteratively or recursively. Could you implement both?
 // /**
-//  * @param {number[]} nums
-//  * @param {number} target
-//  * @return {number[]}
+//  * Definition for singly-linked list.
+//  * function ListNode(val) {
+//  *     this.val = val;
+//  *     this.next = null;
+//  * }
 //  */
-// UPDATE (2016/2/13):
-// The return format had been changed to zero-based indices. Please read the above updated description carefully.
-var twoSum = function(nums, target) {
-  var first = 0;
-  var second = 0;
-  var map = new Map();
-  for (var i = 0; i < nums.length; i++) {
-    if ((map.has(nums[i])) && (nums[i] === target/2)) {
-      second = i;
-      first = map.get(target-nums[i]);
-    }
-    else {
-      map.set(nums[i], i);
-      if (map.has(target-nums[i])) {
-        second = i;
-        if (second !== map.get(target-nums[i])) {
-          first = map.get(target-nums[i]);
-          break;
-        }
-      }
-    }
+// /**
+//  * @param {ListNode} head
+//  * @return {ListNode}
+//  */
+var reverseList = function(head) {
+  if (head === null) {
+    return null;
   }
-  return [first, second];
+  var left = null;
+  while (head !== null) {
+    var right = head.next;
+    head.next = left;
+    left = head;
+    head = right;
+  }
+  return left;
 }
-var nums = [2, 7, 11, 15];
-var target = 9;
-console.log(twoSum(nums, target));
