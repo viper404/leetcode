27f330a1f729bdaebb23ef4601947bd862124d50
vincent peng <viper404@163.com>
2016-09-06 09:55:21 +0800

    Update solution.js

--- 40.CombinationSumII/solution.js
+++ 40.CombinationSumII/solution.js
@@ -1,39 +1,48 @@
-// 1. Two Sum
-// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-// You may assume that each input would have exactly one solution.
-// Example:
-// Given nums = [2, 7, 11, 15], target = 9,
-// Because nums[0] + nums[1] = 2 + 7 = 9,
-// return [0, 1].
+// 40. Combination Sum II
+// Given a collection of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+// Each number in C may only be used once in the combination.
+// Note:
+// All numbers (including target) will be positive integers.
+// The solution set must not contain duplicate combinations.
+// For example, given candidate set [10, 1, 2, 7, 6, 1, 5] and target 8,
+// A solution set is:
+// [
+//   [1, 7],
+//   [1, 2, 5],
+//   [2, 6],
+//   [1, 1, 6]
+// ]
 // /**
-//  * @param {number[]} nums
+//  * @param {number[]} candidates
 //  * @param {number} target
-//  * @return {number[]}
+//  * @return {number[][]}
 //  */
-// UPDATE (2016/2/13):
-// The return format had been changed to zero-based indices. Please read the above updated description carefully.
-var twoSum = function(nums, target) {
-  var first = 0;
-  var second = 0;
-  var map = new Map();
-  for (var i = 0; i < nums.length; i++) {
-    if ((map.has(nums[i])) && (nums[i] === target/2)) {
-      second = i;
-      first = map.get(target-nums[i]);
+var combinationSum2 = function(candidates, target) {
+  var arrs = [];
+  candidates.sort(sortNumber);
+  helper(candidates, target, 0, [], arrs);
+  return arrs;
+}
+function helper(candidates, target, index, arr, arrs) {
+  if (target === 0) {
+    arrs.push(arr);
+    return;
+  }
+  for (var i = index; i < candidates.length; i++) {
+    if ((i > index) && (candidates[i] === candidates[i-1])) {
+      continue;
+    }
+    if (target >= candidates[i]) {
+      helper(candidates, target-candidates[i], i+1, arr.concat(candidates[i]), arrs);
     }
     else {
-      map.set(nums[i], i);
-      if (map.has(target-nums[i])) {
-        second = i;
-        if (second !== map.get(target-nums[i])) {
-          first = map.get(target-nums[i]);
-          break;
-        }
-      }
+      break;
     }
   }
-  return [first, second];
 }
-var nums = [2, 7, 11, 15];
-var target = 9;
-console.log(twoSum(nums, target));
+function sortNumber(a, b) {
+  return a-b;
+}
+var candidates = [10,1,2,7,6,1,5];
+var target = 8;
+console.log(combinationSum2(candidates, target));
