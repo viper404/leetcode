292776f3f00502cb19728058fdc7edacf61453e0
vincent peng <viper404@163.com>
2016-09-14 16:29:09 +0800

    Add a better solution in Discuss of 7.ReverseInteger

--- 7.ReverseInteger/solutioninDiscuss.js
+++ 7.ReverseInteger/solutioninDiscuss.js
@@ -7,10 +7,10 @@
 // If the integer's last digit is 0, what should the output be? ie, cases such as 10, 100.
 // Did you notice that the reversed integer might overflow? Assume the input is a 32-bit integer, then the reverse of 1000000003 overflows. How should you handle such cases?
 // For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
-// /**
-//  * @param {number} x
-//  * @return {number}
-//  */
+/**
+ * @param {number} x
+ * @return {number}
+ */
 // Update (2014-11-10):
 // Test cases had been added to test the overflow behavior.
 var reverse = function(x) {
