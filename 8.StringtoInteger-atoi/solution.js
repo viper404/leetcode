881fcc26da00d29c6239254d657dc0737706359c
vincent peng <viper404@163.com>
2016-09-14 20:38:34 +0800

    My solution of 8.StringtoInteger-atoi

--- 8.StringtoInteger-atoi/solution.js
+++ 8.StringtoInteger-atoi/solution.js
@@ -14,10 +14,10 @@
 // Update (2015-02-10):
 // The signature of the C++ function had been updated.
 // If you still see your function signature accepts a const char * argument, please click the reload button to reset your code definition.
-// /**
-//  * @param {string} str
-//  * @return {number}
-//  */
+/**
+ * @param {string} str
+ * @return {number}
+ */
 var myAtoi = function(str) {
   var patt = /(\+)*(\-)*(\d)*/g;
   var temp = str.match(patt);
