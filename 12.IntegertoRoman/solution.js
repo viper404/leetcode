b9bcc14222aa27c62091c76024a657726917c6de
vincent peng <viper404@163.com>
2016-09-15 11:53:16 +0800

    My solution of 12.IntegertoRoman

--- 12.IntegertoRoman/solution.js
+++ 12.IntegertoRoman/solution.js
@@ -1,10 +1,10 @@
 // 12. Integer to Roman
 // Given an integer, convert it to a roman numeral.
 // Input is guaranteed to be within the range from 1 to 3999.
-// /**
-//  * @param {number} num
-//  * @return {string}
-//  */
+/**
+ * @param {number} num
+ * @return {string}
+ */
 var intToRoman = function(num) {
   var arr = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
   var str = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
