bc9b8df1cad5eb09cbab38b6248dfeaf373dda7e
vincent peng <viper404@163.com>
2016-09-15 11:53:43 +0800

    Add a better solution in Discuss of 12.IntegertoRoman

--- 12.IntegertoRoman/solutioninDiscuss.js
+++ 12.IntegertoRoman/solutioninDiscuss.js
@@ -1,10 +1,10 @@
 // 12. Integer to Roman
 // Given an integer, convert it to a roman numeral.
 // Input is guaranteed to be within the range from 1 to 3999.
-// /**
-//  * @param {number} num
-//  * @return {string}
-//  */
+/**
+ * @param {number} num
+ * @return {string}
+ */
 var intToRoman = function(num) {
   var M = ["", "M", "MM", "MMM"];
   var C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
