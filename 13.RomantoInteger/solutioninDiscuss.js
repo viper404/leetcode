ed753c1ba90a5c2635fe54f0cbdef6f940fbb10a
vincent peng <viper404@163.com>
2016-09-16 11:50:17 +0800

    Add a better solution in Discuss of 13.RomantoInteger

--- 13.RomantoInteger/solutioninDiscuss.js
+++ 13.RomantoInteger/solutioninDiscuss.js
@@ -1,10 +1,10 @@
 // 13. Roman to Integer
 // Given a roman numeral, convert it to an integer.
 // Input is guaranteed to be within the range from 1 to 3999.
-// /**
-//  * @param {string} s
-//  * @return {number}
-//  */
+/**
+ * @param {string} s
+ * @return {number}
+ */
 var romanToInt = function(s) {
   var map = new Map();
   map.set("I", 1);
