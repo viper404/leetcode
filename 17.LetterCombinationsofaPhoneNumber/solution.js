832d66d0412962fd9a2c57dc6c4f867adfb0296d
vincent peng <viper404@163.com>
2016-09-18 17:13:06 +0800

    My solution of 17.LetterCombinationsofaPhoneNumber

--- 17.LetterCombinationsofaPhoneNumber/solution.js
+++ 17.LetterCombinationsofaPhoneNumber/solution.js
@@ -5,10 +5,10 @@
 // Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
 // Note:
 // Although the above answer is in lexicographical order, your answer could be in any order you want.
-// /**
-//  * @param {string} digits
-//  * @return {string[]}
-//  */
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
 var letterCombinations = function(digits) {
   var arrs = [[], ["*"], ["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"],
   ["j", "k", "l"], ["m", "n", "o"], ["p", "q", "r", "s"], ["t", "u", "v"], ["w", "x", "y", "z"]];
