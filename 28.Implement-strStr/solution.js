b5449b85e23507cf27d192ffbb4f07b7ed787209
vincent peng <viper404@163.com>
2016-09-19 11:28:00 +0800

    My solution of 28.Implement-strStr

--- 28.Implement-strStr/solution.js
+++ 28.Implement-strStr/solution.js
@@ -1,11 +1,11 @@
 // 28. Implement strStr()
 // Implement strStr().
 // Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
-// /**
-//  * @param {string} haystack
-//  * @param {string} needle
-//  * @return {number}
-//  */
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
 var strStr = function(haystack, needle) {
   if (needle === "") {
     return 0;
