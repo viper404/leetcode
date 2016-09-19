973e5a9282485705e4071123a3f8b9bd270a1b4c
vincent peng <viper404@163.com>
2016-09-19 11:09:51 +0800

    My solution of 21.MergeTwoSortedLists

--- 21.MergeTwoSortedLists/solution.js
+++ 21.MergeTwoSortedLists/solution.js
@@ -1,18 +1,18 @@
 // 21. Merge Two Sorted Lists
 // Merge two sorted linked lists and return it as a new list.
 // The new list should be made by splicing together the nodes of the first two lists.
-// /**
-//  * Definition for singly-linked list.
-//  * function ListNode(val) {
-//  *     this.val = val;
-//  *     this.next = null;
-//  * }
-//  */
-// /**
-//  * @param {ListNode} l1
-//  * @param {ListNode} l2
-//  * @return {ListNode}
-//  */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
 var mergeTwoLists = function(l1, l2) {
   if ((l1 === null) && (l2 === null)) {
     return [];
