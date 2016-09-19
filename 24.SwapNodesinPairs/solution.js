a1cb77ef553b8a40571db72e4e2f2f511539f58f
vincent peng <viper404@163.com>
2016-09-19 11:18:07 +0800

    My solution of 24.SwapNodesinPairs

--- 24.SwapNodesinPairs/solution.js
+++ 24.SwapNodesinPairs/solution.js
@@ -3,17 +3,17 @@
 // For example,
 // Given 1->2->3->4, you should return the list as 2->1->4->3.
 // Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
-// /**
-//  * Definition for singly-linked list.
-//  * function ListNode(val) {
-//  *     this.val = val;
-//  *     this.next = null;
-//  * }
-//  */
-// /**
-//  * @param {ListNode} head
-//  * @return {ListNode}
-//  */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
 var swapPairs = function(head) {
   if (head === null) {
     return null;
