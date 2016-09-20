5e91b1b53f7db56c1d21cdbf1467a7c83e7130f2
vincent peng <viper404@163.com>
2016-09-20 11:59:15 +0800

    Add a better solution in Discuss of 31.NextPermutation

--- 31.NextPermutation/solutioninDiscuss.js
+++ 31.NextPermutation/solutioninDiscuss.js
@@ -6,10 +6,10 @@
 // 1,2,3 → 1,3,2
 // 3,2,1 → 1,2,3
 // 1,1,5 → 1,5,1
-// /**
-//  * @param {number[]} nums
-//  * @return {void} Do not return anything, modify nums in-place instead.
-//  */
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
 var nextPermutation = function(nums) {
   var k = -1;
   for (var i = nums.length-2; i >= 0; i--) {
